736890117ecb92d42373088d31ac8246e3c345c2
Matheus Cunha <119085549+matheus97mco@users.noreply.github.com>
2023-01-05 00:04:24 -0300

    Create 8.sql

---
+++ SQL/restrição_manipulação/8.sql
@@ -0,0 +1,17 @@
+INSERT INTO Matriculas (matricula,cadastro,componente,aluno,curso) VALUES('7','07/06/2022','7','2','2')  
+
+INSERT INTO Matriculas (matricula,cadastro,componente,aluno,curso) VALUES('8','07/06/2022','3','2','2')  
+
+INSERT INTO Matriculas (matricula,cadastro,componente,aluno,curso) VALUES('9','07/06/2022','10','2','2')  
+
+INSERT INTO Matriculas (matricula,cadastro,componente,aluno,curso) VALUES('10','07/06/2022','7','5','2')  
+
+INSERT INTO Matriculas (matricula,cadastro,componente,aluno,curso) VALUES('11','07/06/2022','3','5','2')  
+
+INSERT INTO Matriculas (matricula,cadastro,componente,aluno,curso)  VALUES('12','07/06/2022','10','5','2')  
+
+INSERT INTO Matriculas (matricula,cadastro,componente,aluno,curso) VALUES('13','07/06/2022','7','3','2')  
+
+INSERT INTO Matriculas (matricula,cadastro,componente,aluno,curso) VALUES('14','07/06/2022','3','3','2')  
+
+INSERT INTO Matriculas (matricula,cadastro,componente,aluno,curso) VALUES('15','07/06/2022','10','3','2')  	
