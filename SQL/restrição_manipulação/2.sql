3382dc58191eb1e77c6454c92b45ca6b34dba730
Matheus Cunha <119085549+matheus97mco@users.noreply.github.com>
2023-01-04 23:59:46 -0300

    Create 2.sql

---
+++ SQL/restrição_manipulação/2.sql
@@ -0,0 +1,9 @@
+INSERT INTO Alunos (aluno,nome,nascimento,cpf,nome_pai,sexo) VALUES('1','Maria','01/01/01','11122233378','Joao','f')  
+
+ INSERT INTO Alunos (aluno,nome,nascimento,cpf,nome_pai,sexo) VALUES('2','Ana','02/01/01','11122244499','Jose','f')  
+ 
+ INSERT INTO Alunos (aluno,nome,nascimento,cpf,nome_pai,sexo) VALUES('3','Ana Maria','03/01/01','11122255533','Pedro','f')  
+ 
+ INSERT INTO Alunos (aluno,nome,nascimento,cpf,nome_pai,sexo) VALUES('4','Matheus','02/02/01','11122233344','Luiz','m')  
+ 
+ INSERT INTO Alunos (aluno,nome,nascimento,cpf,nome_pai,sexo)  VALUES('5','Gabriel','02/01/99','11122233355','Marcos','m')  
