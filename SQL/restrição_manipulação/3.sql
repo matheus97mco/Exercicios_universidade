adfed11f2aba018cca1e4a993c10f6a6e9a768c9
Matheus Cunha <119085549+matheus97mco@users.noreply.github.com>
2023-01-05 00:01:29 -0300

    Create 3.sql

---
+++ SQL/restrição_manipulação/3.sql
@@ -0,0 +1,5 @@
+INSERT INTO Docentes (docente,nome) VALUES('1','Humberto')  
+
+INSERT INTO Docentes (docente,nome) VALUES('2','Luciano')  
+
+INSERT INTO Docentes (docente,nome) VALUES('3','Andre')  
