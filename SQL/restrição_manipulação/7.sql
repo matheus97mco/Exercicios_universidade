5bef6da99e08962d2d4078b2ba412c9b5a215647
Matheus Cunha <119085549+matheus97mco@users.noreply.github.com>
2023-01-05 00:03:57 -0300

    Create 7.sql

---
+++ SQL/restrição_manipulação/7.sql
@@ -0,0 +1,11 @@
+INSERT INTO Matriculas (matricula,cadastro,componente,aluno,curso) VALUES('1','07/06/2022','1','4','1')  
+
+INSERT INTO Matriculas (matricula,cadastro,componente,aluno,curso) VALUES('2','07/06/2022','5','4','1')  
+
+INSERT INTO Matriculas (matricula,cadastro,componente,aluno,curso) VALUES('3','07/06/2022','8','4','1')  
+
+INSERT INTO Matriculas (matricula,cadastro,componente,aluno,curso) VALUES('4','07/06/2022','6','1','1')  
+
+INSERT INTO Matriculas (matricula,cadastro,componente,aluno,curso) VALUES('5','07/06/2022','1','1','1')  
+
+INSERT INTO Matriculas (matricula,cadastro,componente,aluno,curso) VALUES('6','07/06/2022','9','1','1')  
