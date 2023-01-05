cf1b3e8bfad7c4e70ec060c9d1cd0fcf95519b82
Matheus Cunha <119085549+matheus97mco@users.noreply.github.com>
2023-01-05 00:02:09 -0300

    Create 4.sql

---
+++ SQL/restrição_manipulação/4.sql
@@ -0,0 +1,21 @@
+INSERT INTO Componentes (componente,nome,preco,carga_horaria,docente) VALUES('1','Banco de Dados','100','40','1')  
+
+INSERT INTO Componentes (componente,nome,preco,carga_horaria,docente) VALUES('2','Processamento de Imagens','60','20','1')  
+
+INSERT INTO Componentes (componente,nome,preco,carga_horaria,docente) VALUES('3','Administração para Empresas','250','80','1')  
+
+INSERT INTO Componentes (componente,nome,preco,carga_horaria,docente) VALUES('4','Algoritmo','200','20','2')  
+
+INSERT INTO Componentes (componente,nome,preco,carga_horaria,docente) VALUES('5','Segurança','80','30','2')  
+
+INSERT INTO Componentes (componente,nome,preco,carga_horaria,docente) VALUES('6','Redes','250','80','2')  
+
+ALTER TABLE Componentes MODIFY carga_horaria number(6)  
+
+INSERT INTO Componentes (componente,nome,preco,carga_horaria,docente) VALUES('7','Gestão de Negocios','450','100','2')  
+
+INSERT INTO Componentes (componente,nome,preco,carga_horaria,docente) VALUES('8','Estrutura de Dados','60','20','3')  
+
+INSERT INTO Componentes (componente,nome,preco,carga_horaria,docente)  VALUES('9','Programação em Python','150','60','3')  
+
+INSERT INTO Componentes (componente,nome,preco,carga_horaria,docente) VALUES('10','Calculo 1','100','40','3' 
