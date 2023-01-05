d0dd3baaa4881e99664c26cef9d38bd248b37a64
Matheus Cunha <119085549+matheus97mco@users.noreply.github.com>
2023-01-05 00:02:33 -0300

    Create 6.sql

---
+++ SQL/restrição_manipulação/6.sql
@@ -0,0 +1,5 @@
+ALTER TABLE Cursos MODIFY nome VARCHAR2(25)  
+
+INSERT INTO Cursos (curso,nome) VALUES('1','Sistema de Informação')  
+
+INSERT INTO Cursos (curso,nome) VALUES('2','Administração')  
