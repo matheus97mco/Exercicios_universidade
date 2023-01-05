402db4c4cb8b1b6f32b72e8de81b91ba894384ca
Matheus Cunha <119085549+matheus97mco@users.noreply.github.com>
2023-01-05 00:03:25 -0300

    Create 6.sql

---
+++ SQL/restrição_manipulação/6.sql
@@ -0,0 +1,7 @@
+INSERT INTO Alunos_cursos (aluno,curso,status) VALUES('1','1','C')  
+
+INSERT INTO Alunos_cursos (aluno,curso,status) VALUES('4','1','C')  
+
+INSERT INTO Alunos_cursos (aluno,curso,status) VALUES('2','2','C')  
+
+INSERT INTO Alunos_cursos (aluno,curso,status) VALUES('5','2','C') INSERT INTO Alunos_cursos (aluno,curso,status) VALUES('3','2','C')  
